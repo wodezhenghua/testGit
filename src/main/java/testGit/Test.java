c672f2e28ae28ce09d433d2bb396602246abc447
tom_glb <goodMorning_glb@hqyj.com>
2019-04-22 00:48:21 +0800

    my first commit

---
+++ src/main/java/testGit/Test.java
@@ -0,0 +1,20 @@
+package testGit;
+/**  
+* Title: Test.java 
+* Description: 
+* Copyright: Copyright (c) 2018    
+* @author zhh  
+* @date 2019年4月22日  
+* @version 1.0  
+*/
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/testGit/TestGit.java
@@ -0,0 +1,12 @@
+package testGit;
+/**  
+* Title: Test.java 
+* Description: 
+* Copyright: Copyright (c) 2018    
+* @author zhh  
+* @date 2019年4月22日  
+* @version 1.0  
+*/
+public class TestGit {
+
+}
